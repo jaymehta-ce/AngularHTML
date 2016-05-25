719d67559a207188467ab2a302b9250a4cf5a440
jaymehta-ce <jaymehta.ce@gmail.com>
2016-05-25 12:09:17 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  
+
   # You can have the root of your site routed with "root"
 
   root 'welcome#index'
-  match "/404", :to => "welcome#index", :via => :all
+  get "/pages/:page" => "pages#show"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
